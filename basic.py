30903de8eed8970298516b98ed49797828e6df57
Thomas Lupin <thomas.lupin@epita.fr>
2019-06-21 19:25:05 +0200

    [COMMANDS] And tranzat and reverse commands

--- basic.py
+++ basic.py
@@ -1,9 +1,17 @@
+import asyncio
+import math
+from random import randint
+
+import typing
+
+import aiohttp
 from discord import utils
 from discord.ext import commands
 from discord.ext.commands import Context
 
 from misc import embed
 from misc.googleapis import YoutubeAPI
+
 
 class Basic(commands.Cog):
     def __init__(self, bot):
@@ -23,3 +31,41 @@ class Basic(commands.Cog):
         if channel is None:
             return await ctx.send("Pas de chaîne YouTube trouvée.")
         return await ctx.send(embed=embed.channel_embed(channel))
+
+    @commands.command()
+    async def tranzat(self, ctx: Context):
+        """Send a random tranzat"""
+
+        async def send_tranz(message):
+            i = math.floor(randint(0, 200)) * 2
+            url_tranz = 'https://tranzat.tk/tranzat/custom/tranzat{}.png'.format(i)
+            if message is None:
+                message = await ctx.send(embed=embed.tranzat_embed(url_tranz))
+            else:
+                await message.edit(embed=embed.tranzat_embed(url_tranz))
+            try:
+                def check(r, u):
+                    return u == ctx.message.author and r.emoji == u"\U0001F500"
+
+                await message.add_reaction(u"\U0001F500")
+                await self.bot.wait_for('reaction_add', timeout=15.0, check=check)
+            except asyncio.TimeoutError:
+                await message.clear_reactions()
+            else:
+                await message.clear_reactions()
+                await send_tranz(message)
+
+        await send_tranz(None)
+
+    @commands.command()
+    async def reverse(self, ctx: Context, url: str):
+        """Google reverse search"""
+        async with ctx.typing():
+            async with aiohttp.ClientSession() as session:
+                async with session.post('https://mrisa-app.herokuapp.com/search',
+                                        json={'image_url': url, 'resized_images': False}) as r:
+                    if r.status == 200:
+                        js = await r.json(content_type=None)
+                        await ctx.send(embed=embed.reverse_embed(js))
+                    else:
+                        await ctx.send("J'ai pas réussi à faire mon job")
--- misc/embed.py
+++ misc/embed.py
@@ -46,3 +46,22 @@ def channel_embed(channel):
     res.set_footer(text=channel['snippet']['description'])
     res.set_thumbnail(url=channel['snippet']['thumbnails']['medium']['url'])
     return res
+
+
+def tranzat_embed(url_tranz):
+    res = embeds.Embed(type="rich",
+                       description="Créez votre tranzat sur [Tranzat Creator](https://tranzat.tk)",
+                       url="https://tranzat.tk")
+    res.set_author(name="Tranzat Creator", url="https://tranzat.tk",
+                   icon_url="https://cdn.discordapp.com/attachments/480416542014701581/480772692908441631/tranzat.png")
+    res.set_image(url=url_tranz)
+    return res
+
+
+def reverse_embed(js):
+    res = embeds.Embed(type="rich",
+                       description=js['descriptions'][0] if len(js['descriptions']) > 0 else "Pas de description",
+                       title=js['titles'][0] if len(js['titles']) > 0 else "Pas de titre")
+    res.set_thumbnail(url=js['similar_images'][0] if len(js['similar_images']) > 0 else "")
+    res.set_footer(text="Cette image me fait penser à : {}".format(js['best_guess']))
+    return res
