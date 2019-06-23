a9b33a51c7ba9283397d7eb3a44f2958fd9868d4
Thomas Lupin <thomas.lupin@epita.fr>
2019-06-23 11:59:08 +0200

    [SPEAK] Speak when join/leave voice channel

--- speak.py
+++ speak.py
@@ -11,30 +11,38 @@ class Speak(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @staticmethod
+    def speak_tts(guild, sentence, lang="fr-FR"):
+        audio = TextToSpeech().process(sentence, lang)
+        if guild.voice_client.is_playing():
+            guild.voice_client.stop()
+        play_audio_bytes(guild, audio)
+
     @commands.command()
     async def speak(self, ctx: Context, *, sentence: str):
         if ctx.voice_client is not None and ctx.voice_client.is_connected():
-            audio = TextToSpeech().process(sentence)
-            if ctx.voice_client.is_playing():
-                ctx.voice_client.stop()
-            play_audio_bytes(ctx.guild, audio)
+            Speak.speak_tts(ctx.guild, sentence)
         else:
             await ctx.send("Tu ne m'entendra pas...")
 
     @commands.Cog.listener()
-    async def on_voice_state_update(self, member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
-        if before.channel == after.channel:
-            return
-        if member.bot:
+    async def on_voice_state_update(self, member: discord.Member, before: discord.VoiceState,
+                                    after: discord.VoiceState):
+        if before.channel == after.channel or member.bot:
             return
         guild = member.guild
         if not guild.voice_client or not guild.voice_client.is_connected() or guild.voice_client.is_playing():
             return
+        if youtube.Queue.get(guild).is_playing():
+            return
+
         voice_channel = guild.voice_client.channel
         if before.channel != voice_channel and after.channel == voice_channel:
             print(member.display_name + " join " + after.channel.name)
+            Speak.speak_tts(guild, "{} à rejoint {}".format(member.display_name, after.channel.name))
         if after.channel != voice_channel and before.channel == voice_channel:
             print(member.display_name + " left " + before.channel.name)
+            Speak.speak_tts(guild, "{} à quitté {}".format(member.display_name, before.channel.name))
 
     @speak.before_invoke
     async def ensure_queue(self, ctx):
