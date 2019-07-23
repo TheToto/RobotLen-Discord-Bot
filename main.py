cffc34f3a24024c800ae02c7ef4215ff69ecfbd4
Thomas Lupin <thomas.lupin@epita.fr>
2019-07-23 13:34:54 +0200

    [SETTINGS] Clean settings

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from commands import launch_discord_bot
 if not discord.opus.is_loaded():
     discord.opus.load_opus('libopus.so')
 
+#  Create a key.json file with KEY_GOOGLE_CONTENT for google apis
 file = open(settings.GOOGLE_APPLICATION_CREDENTIALS, 'w')
 file.write(settings.KEY_GOOGLE_CONTENT)
 file.close()
--- misc/settings.py
+++ misc/settings.py
@@ -4,18 +4,22 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Discord
-DISCORD_KEY = os.environ.get('DISCORD_KEY')
+DISCORD_KEY = os.environ.get('DISCORD_KEY')  # Discord API KEY
 COMMAND_PREFIX = '>'
 
-# Heroku
-WEB_PORT = os.environ.get('PORT', 5000)
+LOG_CHANNEL = os.environ.get('DISCORD_LOG_CHANNEL', None)  # The discord channel to send logs (optional)
+DM_GUILD = os.environ.get('DISCORD_DM_GUILD', None)  # The guild to place DMs (optional)
+DM_SECTION = os.environ.get('DM_SECTION', None)  # The section in DM_GUILD to place DMs (optional)
 
-# API
-GOOGLE_APPLICATION_CREDENTIALS = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+TRUSTED_USERS = os.environ.get('DISCORD_TRUSTED_USERS', '').split(',')  # List of trusted users for advanced commands
+INSULTS = os.environ.get('INSULTS', '').split(',')
+
+# APIs
+GOOGLE_APPLICATION_CREDENTIALS = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')  # For Text to speech (key.json)
 KEY_GOOGLE_CONTENT = os.environ.get('KEY_GOOGLE_CONTENT')
-YOUTUBE_API_KEY = os.environ.get('YOUTUBE_API_KEY')
+YOUTUBE_API_KEY = os.environ.get('YOUTUBE_API_KEY')  # For sub command (show yt channel infos)
 
-# Lavalink
+# Lavalink (For music cog)
 LAVALINK_HOST = os.environ.get('LAVALINK_HOST')
 LAVALINK_PORT = os.environ.get('LAVALINK_PORT')
 LAVALINK_URI = os.environ.get('LAVALINK_URI')
